64be83fa5a44434b453a7b6932f5e185f25419d5
harzaliolfa <75583875+harzaliolfa@users.noreply.github.com>
2025-03-26 01:18:57 +0100

    Time: 13 ms (31.46%) | Memory: 44.8 MB (26.65%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){return false;}
+
+        Map<Character, Integer> s_map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            s_map.merge(c, 1, Integer::sum);
+        }
+        Map<Character, Integer> t_map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            t_map.merge(c, 1, Integer::sum);
+    
+        }
+        System.out.println("s_map"+ s_map);
+        System.out.println("t_map"+ t_map);
+
+        for(Map.Entry<Character,Integer> entry: s_map.entrySet()){
+            if ( !t_map.containsKey(entry.getKey()) || !t_map.get(entry.getKey()).equals(entry.getValue())){
+                return false;
+           }
+        }
+        return true ;
+    }
+}
