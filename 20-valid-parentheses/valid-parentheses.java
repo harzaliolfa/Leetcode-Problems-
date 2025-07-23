86c4a496d79bbf60dafa92d98d5e12cd25014ce8
Olfa ELharzalli <75583875+harzaliolfa@users.noreply.github.com>
2025-07-23 20:09:12 +0100

    Time: 4 ms (11.54%) | Memory: 42.5 MB (5.17%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+Map<String, Integer> openPar = Map.of("(",1,"{",2,"[",3);
+        Map<String, Integer> closePar = Map.of(")",1,"}",2,"]",3);
+        Stack<String> stack = new Stack<String>();
+        if(s.length() == 1) return false;
+        for(int i =0; i< s.length(); i++){
+            String  c = String.valueOf(s.charAt(i));
+            if(openPar.containsKey(c)){
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty() && closePar.containsKey(c)) return false;
+                if(!stack.isEmpty()){
+                    String lastElmnt = stack.peek();
+                    if(openPar.get(lastElmnt) == closePar.get(c)){
+                        stack.pop();
+                    }
+                    else {
+                        stack.push(c);
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
